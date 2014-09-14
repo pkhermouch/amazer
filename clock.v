508576f89f3a1cd450a17a9cd8506daf177bd90d
Paul Khermouch <pkhermouch@gmail.com>
2014-09-14 18:49:37 -0500

    * Fixed indentation in the beginning of clock.v

--- clock.v
+++ clock.v
@@ -1,19 +1,19 @@
 
 module clock(
-    CLK,                 // 50MHz clock
-    KEY,                 // 4 KEYS
-    HEX0,                // 7-segment
-    HEX1,
-    HEX2,
-    HEX3 
+     CLK,                 // 50MHz clock
+     KEY,                 // 4 KEYS
+     HEX0,                // 7-segment
+     HEX1,
+     HEX2,
+     HEX3 
 );
-    input CLK;
-    input [3:0]KEY;
+     input CLK;
+     input [3:0]KEY;
 
-    output [6:0]HEX0;
-    output [6:0]HEX1;
-    output [6:0]HEX2;
-    output [6:0]HEX3;
+     output [6:0]HEX0;
+     output [6:0]HEX1;
+     output [6:0]HEX2;
+     output [6:0]HEX3;
 	 
 	 wire[31:0] STATE0;
 	 
