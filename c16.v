0c09328101ea69f319d849ec3479fd8953b4e462
Josh Eversmann <josh.eversmann@utexas.edu>
2014-09-20 14:25:31 -0500

    Update c16.v

--- c16.v
+++ c16.v
@@ -147,3 +147,30 @@ output		     [6:0]		HEX3;
 
 
 endmodule
+
+module display(NUM, HEX);
+	input[3:0] NUM;
+	
+	output[6:0] HEX;
+	reg[6:0] HEX;
+	
+	always @(*)
+	case (NUM)
+		4'h0 : HEX = 7'b1000000;
+		4'h1 : HEX = 7'b1111001;
+		4'h2 : HEX = 7'b0100100;
+		4'h3 : HEX = 7'b0110000;
+		4'h4 : HEX = 7'b0011001;
+		4'h5 : HEX = 7'b0010010;
+		4'h6 : HEX = 7'b0000010;
+		4'h7 : HEX = 7'b1111000;
+		4'h8 : HEX = 7'b0000000;
+		4'h9 : HEX = 7'b0010000;
+		4'hA : HEX = 7'b0001000;
+		4'hB : HEX = 7'b0000011;
+		4'hC : HEX = 7'b0100111;
+		4'hD : HEX = 7'b0100001;
+		4'hE : HEX = 7'b0000110;
+		4'hF : HEX = 7'b0001110;
+	endcase
+endmodule
